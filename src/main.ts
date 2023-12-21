39911af21561c7c9d1a011ce03db16e31f24e274
Arto Gahr <artogahr@gmail.com>
2023-12-21 22:54:43 +0100

    baby steps into learning about javascript objects

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
-// Read the contents of the file
-const text = await Deno.readTextFile("src/input.txt");
+// Specify the path to your text file
+const filePath = 'src/input.txt';
 
-// Print the content to the console
-console.log('File Content:', text);
+// Read the contents of the file
+const text = await Deno.readTextFile(filePath);
+
+let currentPhysicalInterface = null;
+const parsedData: {physicalInterfaces: InterfaceData[]} = {
+    physicalInterfaces: []
+  };
+
+  interface InterfaceData {
+    name: string;
+    data: string;
+  }
+  const sections = text.split('\n\n');
+
+sections.forEach(section => {
+
+  const lines = section.split('\n');
+  const interfaceName = lines[0].split(' ')[0];
+  const interfaceData = lines.slice(1).join('\n');
+  console.log(interfaceName);
+  parsedData.physicalInterfaces.push({ name: interfaceName, data: interfaceData });
+})
+
